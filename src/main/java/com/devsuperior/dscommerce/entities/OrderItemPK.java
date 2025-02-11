b0510a8355444803bf09bb2c52105f92b4897793
rodrigo-vale93 <rodrigopvale@hotmail.com>
2025-02-11 20:33:34 -0300

    Equals hashcode

--- src/main/java/com/devsuperior/dscommerce/entities/OrderItemPK.java
+++ src/main/java/com/devsuperior/dscommerce/entities/OrderItemPK.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Embeddable;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+import java.util.Objects;
+
 @Embeddable
 public class OrderItemPK {
 
@@ -35,4 +37,18 @@ public class OrderItemPK {
     }
 
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OrderItemPK that = (OrderItemPK) o;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(order);
+        result = 31 * result + Objects.hashCode(product);
+        return result;
+    }
 }
--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -6,6 +6,7 @@ import org.aspectj.weaver.ast.Or;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "tb_user")
@@ -87,6 +88,19 @@ public User(Long id, String name, String email, String phone, LocalDate birthDat
 
     public List<Order> getOrders() {
         return orders;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 
 }
